7adefad059bd6c3304e38156ff7d71a6f8155787
Kaustubh <kaustubh.kesarkar@gmail.com>
2020-06-05 19:12:38 -0700

    Deleted logs

--- ARWeatherPlus/Services/WeatherService.swift
+++ ARWeatherPlus/Services/WeatherService.swift
@@ -13,10 +13,7 @@ class WeatherService {
     let WEATHER_API_KEY: String = "{your api key}}"
     
     func getWeather(city: String, completion: @escaping (WeatherData?) -> ()) {
-        
-        print("-- LOG2 url")
-        print("https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(WEATHER_API_KEY)&units=imperial")
-        
+                
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(WEATHER_API_KEY)&units=imperial")
             else {
                 completion(nil)
@@ -29,17 +26,11 @@ class WeatherService {
                 completion(nil)
                 return
             }
-            
-            print("-- LOG2 json data")
-            print(data)
-            
+                        
             let weatherDataResponse = try? JSONDecoder().decode(WeatherData.self, from: data)
             if let weatherData = weatherDataResponse {
-                print("AAA")
-                print(weatherData.dt)
                 completion(weatherData)
             } else {
-                print("BBB")
                 completion(nil)
             }
             
